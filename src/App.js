ed63ff4917ec0beb3cda806638224e52033172a1
Andhika_RK <andhikarestu873@gmail.com>
2023-07-18 22:53:42 +0800

    style

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import './App.css';
-import Header from './components/header';
-import About from './components/about';
-import BannerOne from './components/bannerOne';
-import Contact from './components/contact';
-import Footer from './components/footer';
+import Main from './Main';
 import Form from './components/formPage';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header />
-      <About />
-      <BannerOne />
-      <Contact />
-      <Footer /> */}
-      <Form />
+      <Main />
+      {/* <Form /> */}
     </div>
   );
 }
---
+++ src/Main.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import Header from './components/header';
+import About from './components/about';
+import BannerOne from './components/bannerOne';
+import Contact from './components/contact';
+import Footer from './components/footer';
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+      <About />
+      <BannerOne />
+      <Contact />
+      <Footer />
+
+    </div>
+  )
+}
+
+export default Main
